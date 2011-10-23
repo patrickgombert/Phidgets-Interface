9dcff98682d7eaadb732aa068f0ff8096ee609c6
patrickgombert <patrickgombert@gmail.com>
2011-10-23 01:40:10 -0500

    Improved Makefile

--- Makefile
+++ Makefile
@@ -2,14 +2,25 @@ CC =            g++
 
 CXXFLAGS =	-g -Wall
 
+CFLAGS =        -c -Wall
+
 LIBS =		-lphidget21
 
 TARGET =	readphidget
 
 all: $(TARGET)
 
-$(TARGET): main.cpp phidgetcall.cpp phidgetmap.cpp
+$(TARGET): main.o phidgetcall.o phidgetmap.o
 	$(CC) $(CXXFLAGS) main.cpp phidgetcall.cpp phidgetmap.cpp -o $(TARGET) $(LIBS)
 
+main.o: main.cpp
+	$(CC) $(CFLAGS) main.cpp -o main.o
+
+phidgetcall.o: phidgetcall.cpp
+	$(CC) $(CFLAGS) phidgetcall.cpp -o phidgetcall.o
+
+phidgetmap.o: phidgetmap.cpp
+	$(CC) $(CFLAGS) phidgetmap.cpp -o phidgetmap.o
+
 clean:
-	rm -f $(TARGET)
+	rm -f *.o $(TARGET)
--- main.cpp
+++ main.cpp
@@ -7,8 +7,12 @@ int main(int args, char *argv[]) {
   if(args == 2) {
     cout << c->getValue(argv[1]) << endl;
   } else {
-    cout << "Usage:\n  readphidget <alias>" << endl;
-    cout << args << endl;
+    cout << "Usage:\n  readphidget <alias>\n" << endl;
+    cout << "Define Phidget aliases in mapping.txt" << endl;
+    cout << "Use the following convention:" << endl;
+    cout << "  key:value" << endl;
+    cout << "Where key is the phidget's alias (defined by you)" << endl;
+    cout << "And value is the port that phidget is using on the interface kit" << endl;
   }
   delete c;
   return 0;
