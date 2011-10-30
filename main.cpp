77c4543d4f670c8ab963c4e88838ac1c91e7d577
patrickgombert <patrickgombert@gmail.com>
2011-10-29 19:01:34 -0500

    Improved user feedback

--- main.cpp
+++ main.cpp
@@ -3,17 +3,30 @@
 
 using namespace std;
 
+void printResult(int value, string arg) {
+  if(value == -1) {
+    cout << "No attached phidget named: " + arg << endl;
+  } else {
+    cout << value << endl;
+  }
+}
+
+void printUsage() {
+  cout << "Usage:\n  readphidget <alias>\n" << endl;
+  cout << "Define Phidget aliases in mapping.txt" << endl;
+  cout << "Use the following convention:" << endl;
+  cout << "  key:value" << endl;
+  cout << "Where key is the phidget's alias (defined by you)" << endl;
+  cout << "And value is the port that phidget is using on the interface kit" << endl;
+}
+
 int main(int args, char *argv[]) {
   Call *c = new Call();
   if(args == 2) {
-    cout << c->getValue(argv[1]) << endl;
+    int value = c->getValue(argv[1]);
+    printResult(value, argv[1]);
   } else {
-    cout << "Usage:\n  readphidget <alias>\n" << endl;
-    cout << "Define Phidget aliases in mapping.txt" << endl;
-    cout << "Use the following convention:" << endl;
-    cout << "  key:value" << endl;
-    cout << "Where key is the phidget's alias (defined by you)" << endl;
-    cout << "And value is the port that phidget is using on the interface kit" << endl;
+    printUsage();
   }
   delete c;
   return 0;
