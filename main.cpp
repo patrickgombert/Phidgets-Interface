3fda5984e34556b6e2605baa448a469ca15f1fb5
patrickgombert <patrickgombert@gmail.com>
2011-11-18 20:06:41 -0600

    Trying to narrow down fast cgi configuration problem

--- main.cpp
+++ main.cpp
@@ -3,32 +3,31 @@
 
 using namespace std;
 
-Call *c;
+//Call *c;
 FCGX_Request request;
 
 void initialize() {
-  c = new Call();
+  //c = new Call();
 
   FCGX_Init();
   FCGX_InitRequest(&request, 0, 0);
 }
 
 int main(int args, char *argv[]) {
+  FILE* fp = fopen("/tmp/blah", "w");
+  fprintf(fp, "Hello, Server.\n");
+  fclose(fp);
   initialize();
 
   while(FCGX_Accept_r(&request) == 0) {
-    char* path;
-    path = FCGX_GetParam("PATH", request.envp);
-
     printf("Content-type: text/html\r\n"
            "\r\n"
-           "<h1>FastCGI Hello! (C, fcgi_stdio library)</h1>"
-           "<body>Path Given: %s", path, "</body>");
+           "<h1>FastCGI Hello! (C, fcgi_stdio library)</h1>");
 
     FCGI_SetExitStatus(200);
     FCGX_Finish_r(&request);
   }
 
-  delete c;
+  //delete c;
   return 0;
 }
